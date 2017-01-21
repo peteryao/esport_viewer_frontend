2916c6c29577b3b032c23be9009f5b31e3c5c17f
Peter Yao <peteryao916@gmail.com>
2017-01-21 14:54:02 -0800

    Created esport component

---
+++ src/app/esport/esport.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { EsportComponent } from './esport.component';
+
+describe('EsportComponent', () => {
+  let component: EsportComponent;
+  let fixture: ComponentFixture<EsportComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EsportComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EsportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
