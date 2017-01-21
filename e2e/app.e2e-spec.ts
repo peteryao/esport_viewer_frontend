1b3ed7a26e34ee5cb784fef3697b20908de0c282
angular-cli <angular-cli@angular.io>
2017-01-21 12:49:58 -0800

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EvFrontPage } from './app.po';
+
+describe('ev-front App', function() {
+  let page: EvFrontPage;
+
+  beforeEach(() => {
+    page = new EvFrontPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
