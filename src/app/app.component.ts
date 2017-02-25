0eb6fd5ab72a8772b502ddb24495a58733f88f89
Peter Yao <peteryao916@gmail.com>
2017-02-25 11:18:39 -0800

    Tile testing with data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  tiles = [
+    {text: '1', cols: 3, rows: 1, color: 'lightblue'},
+  ];
+
+  private addGridItem(): void {
+    this.tiles.push({text: 'adding', cols: 3, rows: 1, color: 'lightblue'});
+  }
 }
